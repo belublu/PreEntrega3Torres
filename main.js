9509262b646edab9505c2750508a2219218d68f4
Belen Torres <belentorress@gmail.com>
2023-04-13 19:01:05 -0300

    Desarrollo de carrito de compras en js

--- main.js
+++ main.js
@@ -6,34 +6,48 @@ bienvenida()
 
 
 /* Ingreso de datos personales */
-let nombreUsuario = prompt ("Ingresa tu nombre y apellido")
-nombreUsuario = nombreUsuario.toLowerCase()
-console.log (nombreUsuario)
-while(nombreUsuario == " " || nombreUsuario == false){
-    alert("Nombre y apellido ingresado incorrectamente. Intentalo nuevamente.")
-    nombreUsuario = prompt ("Ingresa tu nombre y apellido")
-    
+nombre = () => {
+  let nombreUsuario = prompt ("Ingresa tu nombre y apellido")
+  nombreUsuario = nombreUsuario.toLowerCase()
+  console.log (nombreUsuario)
+  while(nombreUsuario == " " || nombreUsuario == false){
+      alert("Nombre y apellido ingresado incorrectamente. Intentalo nuevamente.")
+      nombreUsuario = prompt ("Ingresa tu nombre y apellido")
+      
+  }
 }
+nombre()
 
-let edadUsuario = prompt ("Ingresa tu edad")
-console.log (edadUsuario)
-while (edadUsuario <= 18){
-    alert ("No podes continuar con el registro ya que sos menor de edad")
-    edadUsuario = prompt ("Ingrese su edad")
-    console.log (edadUsuario)
+
+edad = () =>{
+  let edadUsuario = prompt ("Ingresa tu edad")
+  console.log (edadUsuario)
+  while (edadUsuario <= 18){
+      alert ("No podes continuar con el registro ya que sos menor de edad")
+      edadUsuario = prompt ("Ingrese su edad")
+      console.log (edadUsuario)
+  }
 }
+edad()
 
-let diaNacUsuario = parseInt(prompt("Ingresa tu día de nacimiento"))
+
+diaNacimiento = () => {
+  let diaNacUsuario = parseInt(prompt("Ingresa tu día de nacimiento"))
 console.log (diaNacUsuario)
 while ((diaNacUsuario <= 0 || diaNacUsuario >= 32) || diaNacUsuario == false || diaNacUsuario == " " || isNaN(diaNacUsuario)){
     alert ("Día ingresado incorrectamente")
     diaNacUsuario = prompt ("Ingrese su día de nacimiento")
     console.log (diaNacUsuario)
 }
+}
+diaNacimiento()
 
-let mesNacUsuario
-let salir = true    
-do {
+
+   
+mesNacimiento = () =>{
+  let mesNacUsuario
+  let salir = true 
+  do {
     mesNacUsuario = prompt ("Ingrese su mes de nacimiento").toLowerCase()
     switch(mesNacUsuario){
         case "enero":
@@ -86,15 +100,19 @@ do {
     }
 } while (salir)
 console.log (mesNacUsuario)
+}
+mesNacimiento()
 
-let anioNacUsuario = parseInt(prompt ("Ingresa tu año de nacimiento"))
+anioNacimiento = () =>{
+  let anioNacUsuario = parseInt(prompt ("Ingresa tu año de nacimiento"))
 console.log (anioNacUsuario)
 while (anioNacUsuario > 2005 || anioNacUsuario == " " || isNaN(anioNacUsuario)){
     alert ("Año ingresado incorrectamente.")
     anioNacUsuario = prompt ("Ingresa tu año de nacimiento")
     console.log (anioNacUsuario)
 }
-
+}
+anioNacimiento()
 
 /* Carrito de compra */
 let totalCarrito = 0;
@@ -104,39 +122,71 @@ function agregarProducto(precio) {
   totalCarrito += cantidad * precio;
 }
 
+
+const carrito = []
 function productos() {
   let agregarOtro = true;
-
   while (agregarOtro) {
     let productosElegido = parseInt(prompt("Ingresa el número que corresponde al producto que queres agregar al carrito de compras.\n 1) Funko Pop! Iron Man $5500 \n 2) Funko Pop! Darth Vader $6500 \n 3) Juego Funko Verse Rick & Morty $14800 \n 4) Pin Spiderman $1200 \n 5) Vinyl Soda Superman $4900 \n 6) Mochila Avengers $25450"));
 
     switch (productosElegido) {
       case 1:
+        carrito.push({
+          valor: 5500,
+          nombreProducto: "Funko Pop! Iron Man",
+          categoria: "Pop",
+          cantidad: agregarProducto(5500)
+        })
         alert("Seleccionaste un Funko Pop! Iron Man y su valor es de $5500");
         agregarProducto(5500);
         break;
 
       case 2:
+        carrito.push({
+          valor: 6500,
+          nombreProducto: "Funko Pop! Darth Vader",
+          categoria: "Pop"
+        })
         alert("Seleccionaste un Funko Pop! Darth Vader y su valor es de $6500");
         agregarProducto(6500);
         break;
 
       case 3:
+        carrito.push({
+          valor: 14800,
+          nombreProducto: "Juego Funko Verse Rick & Morty",
+          categoria: "Juegos"
+        })
         alert("Seleccionaste un Juego Funko Verse Rick & Morty y su valor es de $14800");
         agregarProducto(14800);
         break;
 
       case 4:
+        carrito.push({
+          valor: 1200,
+          nombreProducto: "Pin Spiderman",
+          categoria: "Accesorios"
+        })
         alert("Seleccionaste un Pin Spiderman y su valor es de $1200");
         agregarProducto(1200);
         break;
 
       case 5:
+        carrito.push({
+          valor: 4900,
+          nombreProducto: "Vinyl Soda Superman",
+          categoria: "Vinyl"
+        })
         alert("Seleccionaste un Vinyl Soda Superman y su valor es de $4900");
         agregarProducto(4900);
         break;
 
       case 6:
+        carrito.push({
+          valor: 25450,
+          nombreProducto: "Mochila Avengers",
+          categoria: "Indumentaria"
+        })
         alert("Seleccionaste una Mochila Avengers y su valor es de $25450");
         agregarProducto(25450);
         break;
@@ -148,11 +198,20 @@ function productos() {
 
     agregarOtro = confirm("¿Quieres agregar otro producto?");
   }
-
-  console.log("El total del carrito es de: $" + totalCarrito);
 }
-
 productos();
+
+function mostrarCarrito() {
+  console.log("Productos en el carrito:");
+  carrito.forEach(producto => {
+    console.log("- " + producto.nombreProducto + " (" + producto.categoria + "): $" + producto.valor);
+  });
+  console.log("Total del carrito: $" + totalCarrito);
+  alert("Productos en el carrito:\n" + 
+        carrito.map(producto => "- Nombre: " + producto.nombreProducto + " / Categoria: " + producto.categoria + " / Precio: $" + producto.valor).join("\n") +
+        "\nTotal del carrito: $" + totalCarrito);
+}
+mostrarCarrito()
 
 
 /* Envío */
