ad67ab695f6c824eb8b08caa419c51bc4c600b27
Belen Torres <belentorress@gmail.com>
2023-05-01 21:46:49 -0300

    Moficación en el login y registro de cliente

--- loginRegistroCliente.js
+++ loginRegistroCliente.js
@@ -5,12 +5,14 @@ formLoginCliente.addEventListener("submit", (e)=>{
     const usuarioValido = document.getElementById("usuario").value
     const passwordValido = document.getElementById("password").value
 
-    if (usuarioValido === usuarioValido.toLowerCase()){
+    if (usuarioValido === usuarioValido.toLowerCase() && passwordValido.length >= 8 && passwordValido.length <= 12){
         localStorage.setItem("usuario", usuarioValido)
         localStorage.setItem("password", passwordValido)
         window.location.href = "../pages/tienda.html"
-    }else if(usuarioValido === usuarioValido.toUpperCase()){
+    }else if(usuarioValido === usuarioValido.toUpperCase() || passwordValido.length <8 && passwordValido.length >12){
         console.log("Sólo ingresar minúsculas")
+    }else{
+        console.log("La contraseña debe tener entre 8 y 12 caracteres. Intentalo nuevamente.")
     }
 })
 
@@ -24,15 +26,16 @@ formRegistroCliente.addEventListener("submit", (e)=>{
     const fechaNaciClienteValido = document.getElementById("fechaNacCliente").value
     const fechaPermitida = new Date(2005, 0, 1)
     const fechaNacimiento = new Date(fechaNaciClienteValido)
+    const passwordRegistro = document.getElementById("passwordRegistro").value
 
-    if(edadValido > 19 && fechaNacimiento <= fechaPermitida){
+    if(edadValido > 19 && fechaNacimiento <= fechaPermitida && passwordRegistro.length >= 8 && passwordRegistro.length <= 12){
         localStorage.setItem("nombreApellidoCliente", nombreApellidoValido)
         localStorage.setItem("edadCliente", edadValido)
         localStorage.setItem("fechNacCliente", fechaNaciClienteValido)
         window.location.href = "../pages/tienda.html"
+    }else if(passwordRegistro.length < 8 || passwordRegistro.length > 12){
+        console.log("La contraseña debe tener entre 8 y 12 caracteres. Intentalo nuevamente.")
     }else{
         console.log("No te podés registrar ya que sos menor de 18 años.")
     }
 })
-
-
